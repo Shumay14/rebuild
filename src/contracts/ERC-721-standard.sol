5d0562eab0b78958cbce061fef26348449cd4137
Shumay14 <86043952+Shumay14@users.noreply.github.com>
2022-08-09 23:30:44 +0900

    Update ERC-721-standard.sol

--- src/contracts/ERC-721-standard.sol
+++ src/contracts/ERC-721-standard.sol
@@ -84,21 +84,54 @@ interface ERC721 /* is ERC165 */ {
     /// @param _tokenId The NFT to transfer
     function transactionFrom(address _from, address _to, uint256 _tokenId) external payable;
 
-
-
-
+    /// @notice Set or reaffirm the approved address for an NFT
+    /// @dev The zero address indicates there is no approved address.
+    /// @dev Throws unless `msg.sender`is the current NFT owner, or an authorized operator of the current owner.
+    /// @param _approved The new approved NFT controller
+    /// @param _tokenId The NFT to approve
     function approve(address _approved, uint256 _tokenId) external payable;
 
+
+    /// @notice Enable or disable approval for a third party ("operator") to manage all of `msg.sender` 's assets.
+    /// @dev Emits the ApprovalForAll event. The contract MUST allow multiple operators per owner.
+    /// @param _operator Address to add to the set of authorized operators.
+    /// @param _approved True if the operator is approved, false to revoke approval
     function setApprovalForAll(address _operator, bool _approved) external;
 
+    /// @notice Get the approved address for a single NFT
+    /// @dev Throws if `_tokenId` is not a valid NFT
+    /// @param _tokenId The NFT to find the approved address for
+    /// @return The approved address for this NFT, or the zero address if there is none
     function getApproved(uint256 _tokenId) external view returns (address);
 
-    
+    /// @notice Query if and address is an authorized operator for another address
+    /// @param _owner The address that owns the NFTs
+    /// @param _operator The address that acts on behalf of the owner
+    /// @return True if `_operator` is an approved operator for `_owner`, false otherwise
+    function isApprovedForAll(address _owenr, address _operator) external view returns (bool);
+}
 
+interface ERC165 {
+    /// @notice Query if a contract implements an interface
+    /// @param interfaceID The interface identifier, as specified in ERC-165
+    /// @dev Interface identification is specified in ERC-165.
+    /// This function uses less than 30,000 gas.
+    /// @return `true` if the contract implements `interfaceID` and
+    /// `interfaceID` is not 0xfffffff, `false` otherwise
+    function supportsInterface(bytes4 interfaceID) external view returns (bool);
+}
 
-
-
-
-    
-
+interface ERC721TokenReceiver {
+    /// @notice Handle the receipt of an NFT
+    /// @dev The ERC721 smart contract calls this function on the receipient after a `transfer`.
+    /// This function MAY throw to revert and reject the transfer.
+    /// Return of other than the magic value MUST result in the transaction being reverted.
+    /// @notice The contract address is always the message snedr.
+    /// @param _operator The address which called `safeTransferFrom` function
+    /// @param _from The address which previously owned the token
+    /// @param _tokenId The NFT identifier which is being transferred
+    /// @param _data Additional data with no specified format
+    /// @return `bytes4(keccak256("keccak256("onERC721Received(address, address, unit256, bytes)"))`
+    /// unless throwing
+    function onERC721Received(address _operator, address _from, unit256 _tokenId, bytes _data) external returns(bytes4);
 }
